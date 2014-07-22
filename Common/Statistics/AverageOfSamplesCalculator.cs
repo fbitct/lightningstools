890505ae7cbcb427e0f7f712989dfb25ddfe696e
lightningviper <lightningviper@c362c799-4dda-4fd0-8611-33a35cfb7951>
2014-07-22 02:15:19 +0000

    Refactor

--- Common/Statistics/AverageOfSamplesCalculator.cs
+++ Common/Statistics/AverageOfSamplesCalculator.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Common.Statistics;
+﻿using System.Collections.Generic;
 
-namespace F16CPD.SimSupport.Falcon4
+namespace Common.Statistics
 {
     internal interface IAverageOfSamplesCalculator
     {
