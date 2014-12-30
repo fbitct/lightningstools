5e89f2dbe44bef1e8de5ce51b961d1d2852b2be9
lightningviper <lightningviper@c362c799-4dda-4fd0-8611-33a35cfb7951>
2014-12-30 07:23:32 +0000

    Adding initial support in SimLinkup for AMI 900278002 Simulated Primary ADI - first checkin includes pitch & roll mappings only.

---
+++ SimLinkup/HardwareSupport/AMI/AMI9002780-02HardwareSupportModule.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Remoting;
+using Common.HardwareSupport;
+using Common.MacroProgramming;
+using Common.Math;
+using log4net;
+
+namespace SimLinkup.HardwareSupport.AMI
+{
+    //AMI 9002780-02 Primary ADI
+    public class AMI900278002HardwareSupportModule : HardwareSupportModuleBase, IDisposable
+    {
+        #region Class variables
+
+        private static readonly ILog _log = LogManager.GetLogger(typeof(AMI900278002HardwareSupportModule));
+
+        #endregion
+
+        #region Instance variables
+
+        private bool _isDisposed;
+ 
+        private AnalogSignal _pitchInputSignal;
+        private AnalogSignal.AnalogSignalChangedEventHandler _pitchInputSignalChangedEventHandler;
+        private AnalogSignal _pitchOutputSignal;
+        private AnalogSignal _rollCosOutputSignal;
+        private AnalogSignal _rollInputSignal;
+        private AnalogSignal.AnalogSignalChangedEventHandler _rollInputSignalChangedEventHandler;
+        private AnalogSignal _rollSinOutputSignal;
+
+        #endregion
+
+        #region Constructors
+
+        private AMI900278002HardwareSupportModule()
+        {
+            CreateInputSignals();
+            CreateOutputSignals();
+            CreateInputEventHandlers();
+            RegisterForInputEvents();
+        }
+
+        public override string FriendlyName
+        {
+            get { return "AMI P/N 9002780-02 - Indicator - Simulated Attitude Director Indicator"; }
+        }
+
+        public static IHardwareSupportModule[] GetInstances()
+        {
+            var toReturn = new List<IHardwareSupportModule>();
+            toReturn.Add(new AMI900278002HardwareSupportModule());
+            try
+            {
+                var hsmConfigFilePath = Path.Combine(Util.ApplicationDirectory,
+                                                     "AMI900278002HardwareSupportModuleConfig.config");
+                var hsmConfig =
+                    AMI900278002HardwareSupportModuleConfig.Load(hsmConfigFilePath);
+            }
+            catch (Exception e)
+            {
+                _log.Error(e.Message, e);
+            }
+            return toReturn.ToArray();
+        }
+
+        #endregion
+
+        #region Virtual Method Implementations
+
+        public override AnalogSignal[] AnalogInputs
+        {
+            get { return new[] { _pitchInputSignal, _rollInputSignal }; }
+        }
+
+        public override DigitalSignal[] DigitalInputs
+        {
+            get { return null; }
+        }
+
+        public override AnalogSignal[] AnalogOutputs
+        {
+            get { return new[] { _pitchOutputSignal, _rollSinOutputSignal, _rollCosOutputSignal }; }
+        }
+
+        public override DigitalSignal[] DigitalOutputs
+        {
+            get { return null; }
+        }
+
+        #endregion
+
+        #region Signals Handling
+
+        #region Signals Event Handling
+
+        private void CreateInputEventHandlers()
+        {
+            _pitchInputSignalChangedEventHandler =
+                new AnalogSignal.AnalogSignalChangedEventHandler(pitch_InputSignalChanged);
+            _rollInputSignalChangedEventHandler =
+                new AnalogSignal.AnalogSignalChangedEventHandler(roll_InputSignalChanged);
+        }
+
+        private void AbandonInputEventHandlers()
+        {
+            _pitchInputSignalChangedEventHandler = null;
+            _rollInputSignalChangedEventHandler = null;
+        }
+
+        private void RegisterForInputEvents()
+        {
+            if (_pitchInputSignal != null)
+            {
+                _pitchInputSignal.SignalChanged += _pitchInputSignalChangedEventHandler;
+            }
+            if (_rollInputSignal != null)
+            {
+                _rollInputSignal.SignalChanged += _rollInputSignalChangedEventHandler;
+            }
+        }
+
+        private void UnregisterForInputEvents()
+        {
+            if (_pitchInputSignalChangedEventHandler != null && _pitchInputSignal != null)
+            {
+                try
+                {
+                    _pitchInputSignal.SignalChanged -= _pitchInputSignalChangedEventHandler;
+                }
+                catch (RemotingException)
+                {
+                }
+            }
+            if (_rollInputSignalChangedEventHandler != null && _rollInputSignal != null)
+            {
+                try
+                {
+                    _rollInputSignal.SignalChanged -= _rollInputSignalChangedEventHandler;
+                }
+                catch (RemotingException)
+                {
+                }
+            }
+        }
+
+        #endregion
+
+        #region Signal Creation
+
+        private void CreateInputSignals()
+        {
+            _pitchInputSignal = CreatePitchInputSignal();
+            _rollInputSignal = CreateRollInputSignal();
+        }
+
+        private AnalogSignal CreatePitchInputSignal()
+        {
+            var thisSignal = new AnalogSignal();
+            thisSignal.CollectionName = "Analog Inputs";
+            thisSignal.FriendlyName = "Pitch (Degrees)";
+            thisSignal.Id = "900278002_Pitch_From_Sim";
+            thisSignal.Index = 0;
+            thisSignal.Source = this;
+            thisSignal.SourceFriendlyName = FriendlyName;
+            thisSignal.SourceAddress = null;
+            thisSignal.State = 0;
+            return thisSignal;
+        }
+
+        private AnalogSignal CreateRollInputSignal()
+        {
+            var thisSignal = new AnalogSignal();
+            thisSignal.CollectionName = "Analog Inputs";
+            thisSignal.FriendlyName = "Roll (Degrees)";
+            thisSignal.Id = "900278002_Roll_From_Sim";
+            thisSignal.Index = 0;
+            thisSignal.Source = this;
+            thisSignal.SourceFriendlyName = FriendlyName;
+            thisSignal.SourceAddress = null;
+            thisSignal.State = 0;
+            return thisSignal;
+        }
+
+        private void CreateOutputSignals()
+        {
+            _pitchOutputSignal = CreatePitchOutputSignal();
+            _rollSinOutputSignal = CreateRollSinOutputSignal();
+            _rollCosOutputSignal = CreateRollCosOutputSignal();
+        }
+
+        private AnalogSignal CreatePitchOutputSignal()
+        {
+            var thisSignal = new AnalogSignal();
+            thisSignal.CollectionName = "Analog Outputs";
+            thisSignal.FriendlyName = "Pitch";
+            thisSignal.Id = "900278002_Pitch_To_Instrument";
+            thisSignal.Index = 0;
+            thisSignal.Source = this;
+            thisSignal.SourceFriendlyName = FriendlyName;
+            thisSignal.SourceAddress = null;
+            thisSignal.State = (0.00 + 10.00) / 20.00;
+            return thisSignal;
+        }
+
+        private AnalogSignal CreateRollSinOutputSignal()
+        {
+            var thisSignal = new AnalogSignal();
+            thisSignal.CollectionName = "Analog Outputs";
+            thisSignal.FriendlyName = "Roll SIN";
+            thisSignal.Id = "900278002_Roll_SIN_To_Instrument";
+            thisSignal.Index = 0;
+            thisSignal.Source = this;
+            thisSignal.SourceFriendlyName = FriendlyName;
+            thisSignal.SourceAddress = null;
+            thisSignal.State = (0.00 + 10.00) / 20.00;
+            return thisSignal;
+        }
+
+        private AnalogSignal CreateRollCosOutputSignal()
+        {
+            var thisSignal = new AnalogSignal();
+            thisSignal.CollectionName = "Analog Outputs";
+            thisSignal.FriendlyName = "Roll COS";
+            thisSignal.Id = "900278002_Roll_COS_To_Instrument";
+            thisSignal.Index = 0;
+            thisSignal.Source = this;
+            thisSignal.SourceFriendlyName = FriendlyName;
+            thisSignal.SourceAddress = null;
+            thisSignal.State = (10.00 + 10.00) / 20.00;
+            return thisSignal;
+        }
+
+        private void pitch_InputSignalChanged(object sender, AnalogSignalChangedEventArgs args)
+        {
+            UpdatePitchOutputValues(args.PreviousState);
+        }
+
+        private void roll_InputSignalChanged(object sender, AnalogSignalChangedEventArgs args)
+        {
+            UpdateRollOutputValues(args.PreviousState);
+        }
+
+        private void UpdatePitchOutputValues(double previousPitchDegrees)
+        {
+            if (_pitchInputSignal != null)
+            {
+                var pitchInputDegrees = _pitchInputSignal.State;
+
+                double pitchOutputValue = 0;
+
+                pitchOutputValue = 10.0000 * (pitchInputDegrees / 90.0);
+
+                if (_pitchOutputSignal != null)
+                {
+                    if (pitchOutputValue < -10)
+                    {
+                        pitchOutputValue = -10;
+                    }
+                    else if (pitchOutputValue > 10)
+                    {
+                        pitchOutputValue = 10;
+                    }
+
+                    _pitchOutputSignal.State = ((pitchOutputValue + 10.0000) / 20.0000);
+                }
+
+            }
+        }
+
+        private void UpdateRollOutputValues(double previousRollDegrees)
+        {
+            if (_rollInputSignal != null)
+            {
+                var rollInputDegrees = _rollInputSignal.State;
+
+                double rollSinOutputValue = 0;
+                double rollCosOutputValue = 0;
+
+                rollSinOutputValue = 10.0000 * Math.Sin(rollInputDegrees * Constants.RADIANS_PER_DEGREE);
+                rollCosOutputValue = 10.0000 * Math.Cos(rollInputDegrees * Constants.RADIANS_PER_DEGREE);
+
+                SimLinkup.UI.frmMain.SharedRuntime.InhibitOutputModuleUpdates();
+                if (_rollSinOutputSignal != null)
+                {
+                    if (rollSinOutputValue < -10)
+                    {
+                        rollSinOutputValue = -10;
+                    }
+                    else if (rollSinOutputValue > 10)
+                    {
+                        rollSinOutputValue = 10;
+                    }
+
+                    Debug.WriteLine("Roll SIN output:" + _rollSinOutputSignal.State);
+                    _rollSinOutputSignal.State = ((rollSinOutputValue + 10.0000) / 20.0000);
+                }
+
+                if (_rollCosOutputSignal != null)
+                {
+                    if (rollCosOutputValue < -10)
+                    {
+                        rollCosOutputValue = -10;
+                    }
+                    else if (rollCosOutputValue > 10)
+                    {
+                        rollCosOutputValue = 10;
+                    }
+
+                    _rollCosOutputSignal.State = ((rollCosOutputValue + 10.0000) / 20.0000);
+                }
+                SimLinkup.UI.frmMain.SharedRuntime.AllowOutputModuleUpdates();
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Destructors
+
+        /// <summary>
+        ///   Public implementation of IDisposable.Dispose().  Cleans up 
+        ///   managed and unmanaged resources used by this 
+        ///   object before allowing garbage collection
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///   Standard finalizer, which will call Dispose() if this object 
+        ///   is not manually disposed.  Ordinarily called only 
+        ///   by the garbage collector.
+        /// </summary>
+        ~AMI900278002HardwareSupportModule()
+        {
+            Dispose();
+        }
+
+        /// <summary>
+        ///   Private implementation of Dispose()
+        /// </summary>
+        /// <param name = "disposing">flag to indicate if we should actually
+        ///   perform disposal.  Distinguishes the private method signature 
+        ///   from the public signature.</param>
+        private void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    UnregisterForInputEvents();
+                    AbandonInputEventHandlers();
+                }
+            }
+            _isDisposed = true;
+        }
+
+        #endregion
+    }
+}
---
+++ SimLinkup/HardwareSupport/AMI/AMI900278002HardwareSupportModuleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimLinkup.HardwareSupport.AMI
+{
+    [Serializable]
+    public class AMI900278002HardwareSupportModuleConfig
+    {
+        public static AMI900278002HardwareSupportModuleConfig Load(string filePath)
+        {
+            return Common.Serialization.Util.DeserializeFromXmlFile<AMI900278002HardwareSupportModuleConfig>(filePath);
+        }
+
+        public void Save(string filePath)
+        {
+            Common.Serialization.Util.SerializeToXmlFile(this, filePath);
+        }
+    }
+}
