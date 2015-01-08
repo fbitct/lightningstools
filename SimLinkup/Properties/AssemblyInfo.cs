2534caa16346e6e6c4cbef5963ff85fad7bcf1c1
lightningviper <lightningviper@c362c799-4dda-4fd0-8611-33a35cfb7951>
2015-01-08 12:19:05 +0000

    Adding setup project for SimLinkup

--- SimLinkup/Properties/AssemblyInfo.cs
+++ SimLinkup/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using log4net.Config;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
