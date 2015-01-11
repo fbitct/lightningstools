02514702abf8fdb351a255962e641447da96bc0d
lightningviper <lightningviper@c362c799-4dda-4fd0-8611-33a35cfb7951>
2015-01-11 10:55:58 +0000

    Refactor

--- MasterBuild/BuildAll.bat
+++ MasterBuild/BuildAll.bat
@@ -1,51 +1,53 @@
+@ECHO OFF
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\AnalogDevices\AnalogDevices.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\BIUSBWrapper\BIUSBWrapper.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\Common\Common.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\F16CPD\F16CPD.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\F4KeyFile\F4KeyFile.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\F4KeyFileEditor\F4KeyFileEditor.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\F4ResourceFileEditor\F4ResourceFileEditor.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\F4SharedMem\F4SharedMem.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\F4SharedMemMirror\F4SharedMemMirror.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\F4TexSharedMem\F4TexSharedMem.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\F4TexSharedMemTester\F4TexSharedMemTester.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\F4Utils\F4Utils.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\F4Utils\Speech\F4Utils.Speech.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\JoyMapper\JoyMapper.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\LightningGauges\LightningGauges.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\Lzss\Lzss.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\MasterBuild\BuildAll.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\MFDExtractor\MFDExtractor.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\PHCC\PHCC.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\PhccDeviceManager\PhccDeviceManager.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\PhccTestTool\PhccTestTool\PhccTestTool.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\PPJoyWrapper\PPJoyWrapper.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\SimLinkup\SimLinkup.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\SpeexInvoke\SpeexInvoke.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\TlkTool\TlkTool.sln
-pause
+IF ERRORLEVEL 1 GOTO END
 "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 ..\TlkTool\UI\TlkTool.UI\TlkTool.UI.sln
+:END
