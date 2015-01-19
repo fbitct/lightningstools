e560bd4fad9551bc1cfa57723cbe82d020051cd7
lightningviper <lightningviper@c362c799-4dda-4fd0-8611-33a35cfb7951>
2015-01-19 08:20:06 +0000

    Refactor

---
+++ LightningGauges/LightningGauges/Renderers/F16/AzimuthIndicator/EWSDispenserStatusRenderer.cs
@@ -0,0 +1,45 @@
+using System.Drawing;
+
+namespace LightningGauges.Renderers.F16.AzimuthIndicator
+{
+    internal static class EWSDispenserStatusRenderer
+    {
+        internal static void DrawEWSDispenserStatus(Graphics gfx, RectangleF goNogoRect, StringFormat miscTextStringFormat,
+            Color warnColor, Color okColor, RectangleF rdyRectangle, RectangleF pflRectangle, Color scopeGreenColor, Font font, InstrumentState instrumentState)
+        {
+            if (instrumentState.EWSGo)
+            {
+                var legendColor = scopeGreenColor;
+                Brush legendBrush = new SolidBrush(legendColor);
+                StringRenderer.DrawString(gfx, "GO", font, legendBrush, goNogoRect, miscTextStringFormat);
+            }
+            else if (instrumentState.EWSNoGo)
+            {
+                var legendColor = warnColor;
+                Brush legendBrush = new SolidBrush(legendColor);
+                StringRenderer.DrawString(gfx, "NOGO", font, legendBrush, goNogoRect, miscTextStringFormat);
+            }
+            else if (instrumentState.EWSDispenseReady)
+            {
+                var legendColor = scopeGreenColor;
+                Brush legendBrush = new SolidBrush(legendColor);
+                StringRenderer.DrawString(gfx, "DISP", font, legendBrush, goNogoRect, miscTextStringFormat);
+            }
+
+
+            if (instrumentState.EWSDispenseReady)
+            {
+                var legendColor = okColor;
+                Brush legendBrush = new SolidBrush(legendColor);
+                StringRenderer.DrawString(gfx, "RDY", font, legendBrush, rdyRectangle, miscTextStringFormat);
+            }
+
+            if (instrumentState.EWSDegraded) //draw PFL legend 
+            {
+                var legendColor = warnColor;
+                Brush legendBrush = new SolidBrush(legendColor);
+                StringRenderer.DrawString(gfx, "PFL", font, legendBrush, pflRectangle, miscTextStringFormat);
+            }
+        }
+    }
+}
---
+++ LightningGauges/LightningGauges/Renderers/F16/AzimuthIndicator/MissileActivitySymbolRenderer.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace LightningGauges.Renderers.F16.AzimuthIndicator
+{
+    internal static class MissileActivitySymbolRenderer
+    {
+        internal static void DrawMissileActivitySymbol(Graphics gfx, int backgroundWidth, int backgroundHeight, Pen emitterPen, int width)
+        {
+            if (DateTime.Now.Millisecond%250 >= 125) return;
+            //draw missile activity symbol
+            emitterPen.DashStyle = DashStyle.Dash;
+            gfx.DrawEllipse(emitterPen,
+                new RectangleF((backgroundWidth/2.0f) + 4, (backgroundHeight/2.0f) + 4, width - 8, width - 8));
+            emitterPen.DashStyle = DashStyle.Solid;
+        }
+    }
+}
