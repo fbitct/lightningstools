002964c0291282fe2f7622dbe3ddd899d258beb4
lightningviper <lightningviper@c362c799-4dda-4fd0-8611-33a35cfb7951>
2015-02-01 09:51:01 +0000

    Adding more F4 Utils file readers

--- F4Utils/Campaign/F4Structs.cs
+++ F4Utils/Campaign/F4Structs.cs
@@ -4938,4 +4938,13 @@ namespace F4Utils.Campaign
        public int weaponType;                        // SMS Weapon Type
        public int dataIdx;                           // Aditional characteristics data file
     } 
+
+    public class SimACDefType
+    {
+       public int  combatClass;                      // What type of combat does it do?
+       public int  airframeIdx;                      // Index into airframe tables
+       public int  signatureIdx;                     // Index into signature tables (IR only for now)
+       public int[]  sensorType=new int[5];          // Sensor Types
+       public int[]  sensorIdx=new int[5];           // Index into sensor data tables
+    } 
 }
