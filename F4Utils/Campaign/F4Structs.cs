c55010914eb2b8c720081af8c873357a4751e4a9
lightningviper <lightningviper@c362c799-4dda-4fd0-8611-33a35cfb7951>
2015-02-01 09:06:09 +0000

    Adding more F4 Utils file readers

--- F4Utils/Campaign/F4Structs.cs
+++ F4Utils/Campaign/F4Structs.cs
@@ -4869,4 +4869,28 @@ namespace F4Utils.Campaign
         public const float PT_DEFAULT_SIZE = 10000.0f;
     }
 
+    public enum AltLethality
+    {
+        HIGH_ALT_LETHALITY = 0, 
+        LOW_ALT_LETHALITY, 
+        NUM_ALT_LETHALITY 
+    }
+    public class RadarDataType
+    {
+	    public int RWRsound;			            // Which sound plays in the RWR
+	    public short RWRsymbol;			            // Which symbol shows up on the RWR
+	    public short RDRDataInd;		            // Index into radar data files
+	    public float[] Lethality = new float[(int)AltLethality.NUM_ALT_LETHALITY];	// Lethality against low altitude targets
+	    public float NominalRange;					// Detection range against F16 sized target
+	    public float BeamHalfAngle;					// radians (degrees in file)
+	    public float ScanHalfAngle;					// radians (degrees in file)
+	    public float SweepRate;						// radians/sec (degrees in file)
+	    public uint CoastTime;						// ms to hold lock on faded target (seconds in file)
+	    public float LookDownPenalty;				// degrades SN ratio
+	    public float JammingPenalty;			    // degrades SN ratio
+	    public float NotchPenalty;					// degrades SN ratio
+	    public float NotchSpeed;					// ft/sec (kts in file)
+	    public float ChaffChance;					// Base probability a bundle of chaff will decoy this radar
+	    public short flag;							// 0x01 = NCTR capable
+    }
 }
