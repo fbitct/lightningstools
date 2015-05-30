887ffdbe019552ce84e8548f53a2fa88ef5fb92b
lightningviper <lightningviper@c362c799-4dda-4fd0-8611-33a35cfb7951>
2015-05-30 06:48:59 +0000

    Adding decimal formatting extension method to Common/Math

---
+++ Common/Math/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Math
+{
+    public static class ExtensionMethods
+    {
+        public static string FormatDecimal(this float toConvert, int decimalPlaces)
+        {
+            var rounded = System.Math.Round(toConvert, decimalPlaces, MidpointRounding.AwayFromZero);
+            var formatString = "{0:0." + new String('0', decimalPlaces) + "}";
+            return String.Format(formatString, rounded); 
+        }
+
+    }
+}
