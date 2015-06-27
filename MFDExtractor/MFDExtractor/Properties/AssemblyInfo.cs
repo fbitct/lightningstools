2f5b2bc3155b93f413cecf96a2bbc2ef3bff5426
lightningviper <lightningviper@c362c799-4dda-4fd0-8611-33a35cfb7951>
2015-06-27 23:38:56 +0000

    MFDE: Release v0.6.1.0

--- MFDExtractor/MFDExtractor/Properties/AssemblyInfo.cs
+++ MFDExtractor/MFDExtractor/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using log4net.Config;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.6.1.0")]
+[assembly: AssemblyFileVersion("0.6.1.0")]
