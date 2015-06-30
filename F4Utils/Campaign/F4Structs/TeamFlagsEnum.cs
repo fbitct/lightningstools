d1356bec811a4ed56f82413866fd4cb218ecd423
lightningviper <lightningviper@c362c799-4dda-4fd0-8611-33a35cfb7951>
2015-06-30 22:26:36 +0000

    F4Utils: adding some campaign types, constants & enums

---
+++ F4Utils/Campaign/F4Structs/TeamFlagsEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F4Utils.Campaign.F4Structs
+{
+    // Team flags
+    public enum TeamFlagsEnum
+    {
+        TEAM_ACTIVE = 0x01,	// Set if team is being used
+        TEAM_HASSATS = 0x02,	// Has satelites
+        TEAM_UPDATED = 0x04,	// We've gotten remote data for this team
+        TEAM_PLAYERPRIO = 0x08,	// Dunc 20111107: Team priorities managed by player, not HQ (i.e "set by HQ" is OFF)
+        TEAM_MEMBER_HASSATS = 0x10,	// BMS - Biker - If team member (who has joind the team) has access to satellite data
+    };
+}
