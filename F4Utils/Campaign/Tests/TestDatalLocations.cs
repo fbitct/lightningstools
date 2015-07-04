1d228ec9c7e51556ff0eb557caaa5dbc1651cb5a
lightningviper <lightningviper@c362c799-4dda-4fd0-8611-33a35cfb7951>
2015-07-04 02:19:16 +0000

    F4Utils: adding unit tests for DdpFile

--- F4Utils/Campaign/Tests/TestDatalLocations.cs
+++ F4Utils/Campaign/Tests/TestDatalLocations.cs
@@ -14,6 +14,7 @@ namespace F4Utils.Campaign.Tests
         public const string TerrainBaseFolder = BMSBasePath + BMSDataFolder + @"Terrdata\";
         public const string TerrainObjectsFolder = TerrainBaseFolder + @"Objects\";
         public const string AcdFile= TerrainObjectsFolder + @"FALCON4.ACD";
+        public const string DdpFile = TerrainObjectsFolder + @"FALCON4.DDP";
         public const string ClassTable = TerrainObjectsFolder + @"FALCON4.CT";
         public const string TestDataOutputFolder = @"C:\lightningtools_testjunk\";
     }
