a13b670a579350a660cdda2a2c18a30a26db8dd1
lightningviper <lightningviper@c362c799-4dda-4fd0-8611-33a35cfb7951>
2015-12-02 05:18:03 +0000

    Fixing command line builds under Visual Studio 2015 and updating build instructions document with VS2015 links and instructions for performing command line builds

--- MasterBuild/Build.bat
+++ MasterBuild/Build.bat
@@ -4,8 +4,8 @@ IF NOT "%ProgFiles86Root%"=="" GOTO start
 SET ProgFiles86Root=%ProgramFiles%
 
 :start
-"%ProgFiles86Root%\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 %1
+"%ProgFiles86Root%\MSBuild\14.0\Bin\MSBuild.exe" /p:Configuration=Release /p:Platform=x86 %1
 IF ERRORLEVEL 1 GOTO END
-"%ProgFiles86Root%\MSBuild\12.0\Bin\MSBuild.exe" /p:Configuration=Debug /p:Platform=x86 %1
+"%ProgFiles86Root%\MSBuild\14.0\Bin\MSBuild.exe" /p:Configuration=Debug /p:Platform=x86 %1
 IF ERRORLEVEL 1 GOTO END
 :END
--- MasterBuild/BuildWithInstallers.bat
+++ MasterBuild/BuildWithInstallers.bat
@@ -4,8 +4,8 @@ IF NOT "%ProgFiles86Root%"=="" GOTO start
 SET ProgFiles86Root=%ProgramFiles%
 
 :start
-"%ProgFiles86Root%\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com"  /Build "Release" %1
+"%ProgFiles86Root%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com"  /Build "Release" %1
 IF ERRORLEVEL 1 GOTO END
-"%ProgFiles86Root%\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com"  /Build "Debug" %1
+"%ProgFiles86Root%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com"  /Build "Debug" %1
 IF ERRORLEVEL 1 GOTO END
 :END
