750fada4a94e89bd0cbdd2e8dab38e005141e3f1
lightningviper <lightningviper@c362c799-4dda-4fd0-8611-33a35cfb7951>
2016-02-14 08:04:01 +0000

    SimLinkup: Added hardware support module for Westin P/N 521993 EPU Fuel guage

---
+++ SimLinkup/HardwareSupport/Westin/Westin521993HardwareSupportModule.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Remoting;
+using Common.HardwareSupport;
+using Common.MacroProgramming;
+using log4net;
+using LightningGauges.Renderers.F16;
+using System.Drawing;
+
+namespace SimLinkup.HardwareSupport.Westin
+{
+    //Westin P/N 521993 F-16 EPU FUEL QTY IND
+    public class Westin521993HardwareSupportModule : HardwareSupportModuleBase, IDisposable
+    {
+        #region Class variables
+
+        private static readonly ILog _log = LogManager.GetLogger(typeof(Westin521993HardwareSupportModule));
+
+        #endregion
+
+        #region Instance variables
+
+        private AnalogSignal _epuFuelPercentageInputSignal;
+        private AnalogSignal.AnalogSignalChangedEventHandler _epuFuelPercentageInputSignalChangedEventHandler;
+        private AnalogSignal _epuFuelPercentageOutputSignal;
+
+        private IEPUFuelGauge _renderer = new EPUFuelGauge();
+
+        private bool _isDisposed;
+
+        #endregion
+
+        #region Constructors
+
+        private Westin521993HardwareSupportModule()
+        {
+            CreateInputSignals();
+            CreateOutputSignals();
+            CreateInputEventHandlers();
+            RegisterForInputEvents();
+        }
+
+        public override string FriendlyName
+        {
+            get { return "Westin P/N 521993 - EPU Fuel Quantity Ind"; }
+        }
+
+        public static IHardwareSupportModule[] GetInstances()
+        {
+            var toReturn = new List<IHardwareSupportModule>();
+            toReturn.Add(new Westin521993HardwareSupportModule());
+            try
+            {
+                var hsmConfigFilePath = Path.Combine(Util.ApplicationDirectory,
+                    "Westin521993HardwareSupportModule.config");
+                var hsmConfig =
+                    Westin521993HardwareSupportModuleConfig.Load(hsmConfigFilePath);
+            }
+            catch (Exception e)
+            {
+                _log.Error(e.Message, e);
+            }
+            return toReturn.ToArray();
+        }
+
+        #endregion
+
+        #region Virtual Method Implementations
+
+        public override AnalogSignal[] AnalogInputs
+        {
+            get { return new[] { _epuFuelPercentageInputSignal }; }
+        }
+
+        public override DigitalSignal[] DigitalInputs
+        {
+            get { return null; }
+        }
+
+        public override AnalogSignal[] AnalogOutputs
+        {
+            get { return new[] { _epuFuelPercentageOutputSignal }; }
+        }
+
+        public override DigitalSignal[] DigitalOutputs
+        {
+            get { return null; }
+        }
+
+        #endregion
+
+        #region Visualization
+        public override void Render(Graphics g, Rectangle destinationRectangle)
+        {
+            _renderer.InstrumentState.FuelRemainingPercent = (float)_epuFuelPercentageInputSignal.State;
+            _renderer.Render(g, destinationRectangle);
+        }
+        #endregion
+
+        #region Signals Handling
+
+        #region Signals Event Handling
+
+        private void CreateInputEventHandlers()
+        {
+            _epuFuelPercentageInputSignalChangedEventHandler =
+                epu_InputSignalChanged;
+        }
+
+        private void AbandonInputEventHandlers()
+        {
+            _epuFuelPercentageInputSignalChangedEventHandler = null;
+        }
+
+        private void RegisterForInputEvents()
+        {
+            if (_epuFuelPercentageInputSignal != null)
+            {
+                _epuFuelPercentageInputSignal.SignalChanged += _epuFuelPercentageInputSignalChangedEventHandler;
+            }
+        }
+
+        private void UnregisterForInputEvents()
+        {
+            if (_epuFuelPercentageInputSignalChangedEventHandler != null && _epuFuelPercentageInputSignal != null)
+            {
+                try
+                {
+                    _epuFuelPercentageInputSignal.SignalChanged -= _epuFuelPercentageInputSignalChangedEventHandler;
+                }
+                catch (RemotingException)
+                {
+                }
+            }
+        }
+
+        #endregion
+
+        #region Signal Creation
+
+        private void CreateInputSignals()
+        {
+            _epuFuelPercentageInputSignal = CreateEPUInputSignal();
+        }
+
+        private void CreateOutputSignals()
+        {
+            _epuFuelPercentageOutputSignal = CreateEPUOutputSignal();
+        }
+
+        private AnalogSignal CreateEPUOutputSignal()
+        {
+            var thisSignal = new AnalogSignal();
+            thisSignal.Category = "Outputs";
+            thisSignal.CollectionName = "Analog Outputs";
+            thisSignal.FriendlyName = "EPU Fuel Quantity %";
+            thisSignal.Id = "521993_EPU_To_Instrument";
+            thisSignal.Index = 0;
+            thisSignal.Source = this;
+            thisSignal.SourceFriendlyName = FriendlyName;
+            thisSignal.SourceAddress = null;
+            thisSignal.State = 0; //volts
+            thisSignal.IsVoltage = true;
+            thisSignal.MinValue = 0;
+            thisSignal.MaxValue = 2;
+            return thisSignal;
+        }
+
+        private AnalogSignal CreateEPUInputSignal()
+        {
+            var thisSignal = new AnalogSignal();
+            thisSignal.Category = "Inputs";
+            thisSignal.CollectionName = "Analog Inputs";
+            thisSignal.FriendlyName = "EPU Fuel Quantity %";
+            thisSignal.Id = "521993_EPU_From_Sim";
+            thisSignal.Index = 0;
+            thisSignal.Source = this;
+            thisSignal.SourceFriendlyName = FriendlyName;
+            thisSignal.SourceAddress = null;
+            thisSignal.State = 0;
+            thisSignal.IsPercentage = true;
+            thisSignal.MinValue = 0;
+            thisSignal.MaxValue = 100;
+
+            return thisSignal;
+        }
+
+        private void epu_InputSignalChanged(object sender, AnalogSignalChangedEventArgs args)
+        {
+            UpdateOutputValues();
+        }
+
+        private void UpdateOutputValues()
+        {
+            if (_epuFuelPercentageInputSignal != null)
+            {
+                var epuInput = _epuFuelPercentageInputSignal.State;
+                double epuOutputValue = 0;
+                if (_epuFuelPercentageOutputSignal != null)
+                {
+                    if (epuInput < 0)
+                    {
+                        epuOutputValue = 0;
+                    }
+                    else if (epuInput > 100)
+                    {
+                        epuOutputValue = 2;
+                    }
+                    else
+                    {
+                        epuOutputValue = ((epuInput / 100) * 2);
+                    }
+
+                    if (epuOutputValue < 0)
+                    {
+                        epuOutputValue = 0;
+                    }
+                    else if (epuOutputValue > 2)
+                    {
+                        epuOutputValue = 2;
+                    }
+
+                    _epuFuelPercentageOutputSignal.State = epuOutputValue;
+                }
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Destructors
+
+        /// <summary>
+        ///     Public implementation of IDisposable.Dispose().  Cleans up
+        ///     managed and unmanaged resources used by this
+        ///     object before allowing garbage collection
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///     Standard finalizer, which will call Dispose() if this object
+        ///     is not manually disposed.  Ordinarily called only
+        ///     by the garbage collector.
+        /// </summary>
+        ~Westin521993HardwareSupportModule()
+        {
+            Dispose();
+        }
+
+        /// <summary>
+        ///     Private implementation of Dispose()
+        /// </summary>
+        /// <param name="disposing">
+        ///     flag to indicate if we should actually
+        ///     perform disposal.  Distinguishes the private method signature
+        ///     from the public signature.
+        /// </param>
+        private void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    UnregisterForInputEvents();
+                    AbandonInputEventHandlers();
+                }
+            }
+            _isDisposed = true;
+        }
+
+        #endregion
+    }
+}
---
+++ SimLinkup/HardwareSupport/Westin/Westin521993HardwareSupportModuleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimLinkup.HardwareSupport.Westin
+{
+    [Serializable]
+    public class Westin521993HardwareSupportModuleConfig
+    {
+        public static Westin521993HardwareSupportModuleConfig Load(string filePath)
+        {
+            return Common.Serialization.Util.DeserializeFromXmlFile<Westin521993HardwareSupportModuleConfig>(filePath);
+        }
+
+        public void Save(string filePath)
+        {
+            Common.Serialization.Util.SerializeToXmlFile(this, filePath);
+        }
+    }
+}
